a50210294b33627812df2930f84cf1573858621d
guillaumepoucet <poucet@simplon-charleville.fr>
2020-05-04 17:03:38 +0200

    edit pell

--- vue/vueEditPost.php
+++ vue/vueEditPost.php
@@ -5,14 +5,13 @@
     <div class="pell-grid">
         <div class="grid-col-0">
             <div id="editor" class="pell">
-                <p><?=$post['post_content']?></p>
             </div>
-            <h3>HTML output:</h3>
-            <pre id="html-output"></pre>
+            <h3>HTML output :</h3>
+            <pre id="html-output"><?=htmlentities($post['post_content'])?></pre>
         </div>
         <div class="output">
             <div style="margin-top:1em;">
-                <h3>Text output:</h3>
+                <h3>Text output :</h3>
                 <div id="text-output"></div>
             </div>
         </div>
